79e252ff89cd7dbc7c178ace5e63686d4dd534af
Nowrin-shaik <shaiknowrin0306@gmail.com>
2024-10-17 10:51:32 +0530

    Create Program_3.java

---
+++ Program_3.java
@@ -0,0 +1,21 @@
+package com.tandemloop.programs;
+
+import java.util.*;
+public class Program_3 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Input:");
+		int a=sc.nextInt();
+		int result=1;
+		a=(a%2!=0)?a:a-1;
+		for(int i=0;i<a;i++) {
+			System.out.print(result);
+			result+=2;
+			if(i<a-1) {
+				System.out.print(",");
+			}
+		}
+		sc.close();
+	}
+}
+
